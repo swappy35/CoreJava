8a1e44657fe1d835010396bb65602ec132fd565d
Swappy <pal35swapnil35@gmail.com>
2021-10-20 09:41:18 +0530

    No change in logic, just cleaned the code.

--- assignment/ExeceptionHandling/Assignment1.java
+++ assignment/ExeceptionHandling/Assignment1.java
@@ -1,33 +1,31 @@
 package assignment.ExeceptionHandling;
 
 public class Assignment1 {
-		static void validate(int salary)throws InvalidAgeException{  
-		     if(salary<=2100)  
-		      throw new InvalidAgeException("You need to work hard"); 
-		     if(salary<=5000)  
-			  throw new InvalidAgeException("Your salary is somehow good");
-		     else  
+		static void validate(int salary)throws SalaryException{  
+		     if(salary>=0 || salary<=2100)  
+		      throw new SalaryException("You need to work hard"); 
+		     if(salary>2100 || salary<=5000)  
+			  throw new SalaryException("Your salary is somehow good");
+		     if(salary>5000 || salary<=9000)  
 		      System.out.println("Salary is very good");  
+//		     else
+//		    	 System.out.println("Invalid salary entered");  
 		   }  
 		     
 		   public static void main(String args[]){  
 		      try{  
-		        validate(2200);  
+		        validate(5000);  
 		      }
 		      catch(Exception m)
 		      {
 		    	  System.out.println("Exception occured: "+ m.getMessage());
-		      }  
-		  
-		      System.out.println("rest of the code...");  
+		      }
 		  }  	
 }
-//This class is created for your own defined exception message
-class InvalidAgeException extends Exception{  // 1
-	
-	//private static final long serialVersionUID = 1L;
 
-	InvalidAgeException(String s){  //2
+class SalaryException extends Exception{  // 1
+	
+	SalaryException(String s){  //2
 	  super(s);  
 	 }  
 } 
